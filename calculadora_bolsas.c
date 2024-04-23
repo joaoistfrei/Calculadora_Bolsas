fb1b154004f7b95c44a0c246c328771935e93990
Joao_Medeiros <joaomedeiros2004@gmail.com>
2024-04-23 00:58:57 -0300

    commit funcionando 1

--- calculadora_bolsas.c
+++ calculadora_bolsas.c
@@ -1,3 +1,10 @@
+//============================== informações relevantes ======================================
+
+#define ALUNO_AUTOR "Joao Pedro Machado Medeiros"
+#define NUSP 14582462
+
+//=============================  bibliotecas utilizadas ======================================
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,72 +20,66 @@ typedef struct {
 
 //============================= declaração das funções =======================================
 
-//funções para o selection sort:
-void trocar(Aluno* a, Aluno* b); //troca o conteúdo dos espaços de memória contidos em *a e *b
+// Funções para o selection sort:
+void trocar(Aluno* a, Aluno* b); // Troca o conteúdo dos espaços de memória contidos em *a e *b
 
-void selection_sort(Aluno lista[], unsigned int tam); //executa o selection sort, dado o vetor "lista" e o tamanho dele "tam"
+void selection_sort(Aluno* lista, int tam); // Executa o selection sort, dado o vetor "lista" e o tamanho dele "tam"
 
-//funções para o merge sort:
-void merge(Aluno lista[], unsigned int e, unsigned int m, unsigned int d); //função para dividir e recolocar dois vetores na ordem os subvetores são E[e,...,m] e o outro D[m+1,...,d]
+// Funções para o merge sort:
+void merge(Aluno* lista, int e, int m, int d); // Função para dividir e recolocar dois vetores na ordem os subvetores são E[e,...,m] e o outro D[m+1,...,d]
 
-void merge_sort(Aluno lista[], unsigned int e, unsigned int d); //executa o merge sort, dado o vetor "lista" e os indices das extremidades
+void merge_sort(Aluno* lista, int e, int d); // Executa o merge sort, dado o vetor "lista" e os indices das extremidades
 
-//=========================================main===============================================
+//======================================= main ===============================================
 
 int main() {
 
-    int T;
-    unsigned int N, M;
-    scanf("%d", &T); //recebe o método de ordenação desejado
-    scanf("%u %u", &N, &M); //recebe o número de alunos e de bolsas disponiveis
+    int T, N, M; // T = metodo de ordenação escolhido | N = número inicial de alunos | M = número inicial de bolsas
+    scanf("%d", &T); // Recebe o método de ordenação desejado
+    scanf("%d %d", &N, &M); // Recebe o número de alunos e de bolsas disponiveis
 
-    Aluno alunos[N]; //matriz para guardar as infos dos alunos
-    fflush(stdin);
-    Aluno temp;
-    int elim = 0;
-    //recebendo medias, numero de rep e nomes
-    for (unsigned int i = 0; i < N; i++) {
-        scanf(" %f %d ", &temp.media, &temp.num_rep);
-        //printf("%.1f %d", alunos[i].media, alunos[i].num_rep);
-        fgets(temp.nome, TAM_MAX, stdin);
-        temp.nome[strcspn(temp.nome, "\n")] = '\0'; //remove o \n no final de cada nome para facilitar a manipulação
-        if(temp.num_rep > 10)
-            elim++;
-        else{   
-            alunos[i-elim] = temp;     
-        }           
+    // Usando calloc para alocar memória para a matriz de alunos
+    Aluno *alunos = (Aluno *)calloc(N, sizeof(Aluno));
+    if (alunos == NULL) {
+        printf("Erro ao alocar memória.\n");
+        return 1;
     }
-    N -= elim;
-    if(M > N)
-        M = N;
+    Aluno temp; // Variável auxiliar para verificar se o aluno está nas condições necessárias (<10 reprovações)
+    int valido = 0; // Variável que recebe o número de alunos aptos a receber a bolsa
+    // Recebendo medias, numero de rep e nomes
+    for (int i = 0; i < N; i++) {
+        scanf("%f %d %s", &temp.media, &temp.num_rep, temp.nome);
+        if(temp.num_rep <= 10){ // Se o aluno puder receber bolsa, é posto na lista "alunos"
+            alunos[valido].media = temp.media;
+            alunos[valido].num_rep = temp.num_rep;
+            strcpy(alunos[valido].nome, temp.nome);
+            valido++;
+        }
+    }
+    N = valido; // Agora N representa o número de alunos aptos a receber bolsa
 
-    //agora, tendo a lista de alunos com notas e reprovações, devemos ordená-la na ordem de preferência definida
+    // Tendo a lista de alunos com notas e reprovações, devemos ordená-la na ordem de preferência definida
     if(T == 1){
         selection_sort(alunos, N);
     } else if(T == 2){
         merge_sort(alunos, 0, N-1);
     }
-    /*
-    for (int i = 0; i < N; i++) {
-        printf("%s ", alunos[i].nome);
-        printf("m = %.1f, r = %d\n", alunos[i].media, alunos[i].num_rep);
+
+    if(M > N) // Como não podem haver mais alunos do que bolsas dadas, é seguro verificar se isso acontece
+        M = N;
+
+    // Devemos checar se há empate nos alunos que estão no limite de ganhar a bolsa
+    while(alunos[M].media == alunos[M-1].media && alunos[M].num_rep == alunos[M-1].num_rep){
+        M++;
     }
-    */
-    unsigned int extra = 0;
-    for(unsigned int i = M - 1; i < N; i++){
-        if(alunos[i].media == alunos[i+1].media && alunos[i].num_rep == alunos[i+1].num_rep){
-            extra++;
-        } else
-            break;
-    }
-    M += extra;
+
     if(M > N)
         M = N;
 
-    //imprimindo o numero de alunos que ganharam e seus nomes
-    printf("%u\n", M);
+    // Imprimindo o numero de alunos que ganharam, seguido de seus nomes
+    printf("%d\n", M);
     for(int i = 0; i < M; i++){
-        printf("%s %.1f %d\n", alunos[i].nome, alunos[i].media, alunos[i].num_rep);
+        printf("%s\n", alunos[i].nome);
     }
     return 0;
 }
@@ -91,7 +92,7 @@ void trocar(Aluno* a, Aluno* b){
     *b = aux;
 }
 
-void selection_sort(Aluno lista[], unsigned int tam) {
+void selection_sort(Aluno* lista, int tam) {
     for (int etapa = 0; etapa < tam - 1; etapa++) {
         int max = etapa;
         for (int i = etapa + 1; i < tam; i++) {
@@ -116,12 +117,13 @@ void selection_sort(Aluno lista[], unsigned int tam) {
     }
 }
 
-void merge(Aluno lista[], unsigned int e, unsigned int m, unsigned int d) {
-    unsigned int n1 = m - e + 1; // tamanho da sub-array 1
-    unsigned int n2 = d - m;     // tamanho da sub-array 2
-    unsigned int i, j, k; 
+void merge(Aluno* lista, int e, int m, int d) {
+    int n1 = m - e + 1; // Tamanho da sub-array da esquerda
+    int n2 = d - m;     // Tamanho da sub-array da direita
+    int i, j, k; 
 
-    Aluno E[n1], D[n2]; // arrays temporárias (Esquerda e Direita)
+    Aluno* E  = (Aluno*)calloc(n1, sizeof(Aluno)); // Arrays temporárias (Esquerda e Direita)
+    Aluno* D  = (Aluno*)calloc(n2, sizeof(Aluno)); // Nesse caso foi usado calloc para nao sobrecarregar a memória stack
 
     // Copiando o conteúdo da array principal para as sub-arrays
     for (i = 0; i < n1; i++)
@@ -133,6 +135,7 @@ void merge(Aluno lista[], unsigned int e, unsigned int m, unsigned int d) {
     i = 0;
     j = 0;
     k = e;
+
     // Comparação e ordenação por notas
     while (i < n1 && j < n2) {
         if (E[i].media > D[j].media) {
@@ -151,15 +154,14 @@ void merge(Aluno lista[], unsigned int e, unsigned int m, unsigned int d) {
                 j++;
             } else { // Em caso de empate no número de reprovações
                 // Desempate em ordem alfabética
-                unsigned int comp = strcmp(E[i].nome, D[j].nome); // Comparação lexicográfica
+                int comp = strcmp(E[i].nome, D[j].nome); // Comparando em ordem alfabética
                 if (comp < 0) { // Se E[i] vem antes de D[j]
                     lista[k] = E[i];
                     i++;
                 } else if (comp > 0) { // Se E[i] vem depois de D[j]
                     lista[k] = D[j];
                     j++;
-                } else { // Se os nomes são iguais
-                    // Ambos os elementos são copiados de volta para o array principal
+                } else { // Se os nomes são iguais, ambos os elementos são copiados de volta para o array principal
                     lista[k] = D[j];
                     j++;
                 }   
@@ -183,14 +185,14 @@ void merge(Aluno lista[], unsigned int e, unsigned int m, unsigned int d) {
     }
 }
 
-void merge_sort(Aluno lista[], unsigned int e, unsigned int d){
+void merge_sort(Aluno* lista, int e, int d){
     if(e < d){
-        unsigned int m = e + (d - e) / 2; // Decidindo onde será dividida a array entre as duas sub-arrays
+        int m = e + (d - e) / 2; // Decidindo onde será dividida a array entre as duas sub-arrays
         // Com recursão, vamos organizar a primeira e segunda metade
         merge_sort(lista, e, m);
         merge_sort(lista, m + 1, d);
 
-        // Agora juntando de volta as duas
+        // Finalmente, juntando de volta as duas partes
         merge(lista, e, m, d);
     }
 }
